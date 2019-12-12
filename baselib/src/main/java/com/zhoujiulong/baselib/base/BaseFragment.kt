56c623801db229abf40fc2c5985659ee7d15f4f0
zhoujiulong <754667445@qq.com>
2019-12-12 15:27:51 +0800

    请求权限封装,方便后期替换

--- baselib/src/main/java/com/zhoujiulong/baselib/base/BaseFragment.kt
+++ baselib/src/main/java/com/zhoujiulong/baselib/base/BaseFragment.kt
@@ -1,5 +1,6 @@
 package com.zhoujiulong.baselib.base
 
+import com.tbruyelle.rxpermissions2.RxPermissions
 import com.zhoujiulong.baselib.LoadingDialog
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
@@ -52,4 +53,26 @@ abstract class BaseFragment<T : BasePresenter<*, *>> : SimpleFragment(),
         mLoadingDialog?.dismiss()
     }
 
+    /**
+     * 请求权限
+     */
+    fun requestPermission(vararg permissions: String, block: (success: Boolean) -> Unit) {
+        val list = mutableListOf<String>()
+        permissions.forEach { list.add(it) }
+        requestPermission(list, 0, true, block)
+    }
+
+    private fun requestPermission(
+        permissions: List<String>, index: Int,
+        allBeforeSuccess: Boolean, block: (success: Boolean) -> Unit
+    ) {
+        val dis = RxPermissions(this).request(permissions[index]).subscribe {
+            if (index == permissions.size - 1) {
+                block(it && allBeforeSuccess)
+            } else {
+                requestPermission(permissions, index + 1, allBeforeSuccess && it, block)
+            }
+        }
+        bindSubscription(dis)
+    }
 }
